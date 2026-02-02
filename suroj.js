7e6f6b4df4ee162b47a287bc34a7efe7fbf2c738
Suroj <student8@sddb.student>
2026-02-02 09:42:50 +0545

    changes

--- suroj.js
+++ suroj.js
@@ -3,7 +3,14 @@ let c=canvas.getContext("2d")
 
 canvasWidth=window.innerWidth
 canvasHeight=window.innerHeight
-
+draw(){
      c.beginPath()
-     c.fillRect(400,400,40,40)
-     c.fillStyle="red"  
+     c.fillRect(120,120,40,40)
+     c.fillStyle="red"
+} 
+ 
+     function animate(){
+        c.clearRect(0,0,canvas.width,canvas.height)
+        
+        draw();
+     }
